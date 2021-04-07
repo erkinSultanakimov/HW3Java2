87968340646ca2124d5f595f93974e404a2ed41a
erkinSultanakimov <erkin.sultanakimov@gmail.com>
2021-04-08 00:19:18 +0600

    Есть еще чем заняться

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,25 +6,25 @@ public class Main {
         BankAccount bankAccount = new BankAccount();
         bankAccount.deposit(20000);
 
-        while (true) {
+
+
             try {
                 bankAccount.withDraw(6000);
-
-            } catch (LimitExeption limit) {
-                System.out.println(limit.getMessage());
-            }
-            try {
                 bankAccount.withDraw((int) bankAccount.getAmount());
-            } catch (LimitExeption l) {
-                System.out.println(l.getMessage());
+            } catch (LimitExeption limitExeption) {
+                System.out.println(limitExeption.getMessage());
+                System.out.println("Доступный баланс: " + bankAccount.getAmount() + "\n Ваш баланс после снятия доступного баланса: " + (bankAccount.getAmount() - bankAccount.getAmount()));
+
             }
 
-            finally {
-                System.out.println("Доступный баланс: " + bankAccount.getAmount() + "\n Ваш баланс после снятия доступного баланса: " + (bankAccount.getAmount() - bankAccount.getAmount()));
-            }
-            break;
-        }
+
+
+
+
+
 
 
     }
 }
+
+
